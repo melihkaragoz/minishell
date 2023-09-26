d38feae6d1171ded1cbf72afb49f6e371baec7de
Melih Karagoz <mkaragoz@k1m39s02.42istanbul.com.tr>
2023-09-26 16:27:13 +0300

    made some examples with redirections

--- src/redirect/ms_redirect.c
+++ src/redirect/ms_redirect.c
@@ -6,44 +6,47 @@
 /*   By: mkaragoz <mkaragoz@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 14:12:46 by mkaragoz          #+#    #+#             */
-/*   Updated: 2023/09/26 14:52:20 by mkaragoz         ###   ########.fr       */
+/*   Updated: 2023/09/26 16:06:16 by mkaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../main/minishell.h"
+void ms_run_herecoc(char **pt, char ***rt);
 void ms_run_outfile(char **pt, int mod);
-void ms_run_infile(char **pt, int mod);
+void ms_run_infile(char **pt);
 void ms_run_redirect(void);
 
 int main(void)
 {
 	ms_run_redirect();
-	return(0);
+	return (0);
 }
 void ms_run_redirect(void)
 {
+	int	stat;
+	char ***rt;
 	char *s = readline("minishell$ ");
 	char **pt = ft_split(s, ' ');
 	char *ex[3];
 
 	ex[0] = ft_strdup(ft_strjoin("/bin/", pt[0]));
-	ex[1] = ft_strdup(pt[1]);
+	ex[1] = 0;
 	ex[2] = NULL;
+	stat = 0;
 	if (!(*pt))
 		return;
-	if ((pt[1] && pt[2]) && !ft_strncmp(pt[2], ">>", 2) && pt[3])
+	if ((pt[0] && pt[1]) && !ft_strncmp(pt[1], ">>", 2) && pt[2])
 		ms_run_outfile(pt, 1);
-	else if ((pt[1] && pt[2]) && !ft_strncmp(pt[2], ">", 1) && pt[3])
+	else if ((pt[0] && pt[1]) && !ft_strncmp(pt[1], ">", 1) && pt[2])
 		ms_run_outfile(pt, 0);
-	else if ((pt[1] && pt[2]) && !ft_strncmp(pt[2], "<<", 2))
-		ms_run_infile(pt, 1);
-	else if ((pt[1] && pt[2]) && !ft_strncmp(pt[2], "<", 1))
-		ms_run_infile(pt, 0);
+	else if ((pt[0] && pt[1]) && !ft_strncmp(pt[1], "<<", 2))
+		ms_run_herecoc(pt);
+	else if ((pt[0] && pt[1]) && !ft_strncmp(pt[1], "<", 1))
+		ms_run_infile(pt);
 	int cp = fork();
 	if (!cp)
-	{
-		execve(ex[0], ex, NULL);
-	}
+		execve(ex[0], ex, 0);
+	waitpid(cp, &stat, (int)NULL);
 }
 
 void ms_run_outfile(char **pt, int mod)
@@ -51,14 +54,21 @@ void ms_run_outfile(char **pt, int mod)
 	int fd;
 
 	if (!mod)
-		fd = open(pt[3], O_RDWR | O_CREAT | O_TRUNC, 0777);
+		fd = open(pt[2], O_RDWR | O_CREAT | O_TRUNC, 0777);
 	else
-		fd = open(pt[3], O_RDWR | O_CREAT | O_APPEND, 0777);
+		fd = open(pt[2], O_RDWR | O_CREAT | O_APPEND, 0777);
 	if (fd == -1)
 		return ((void)printf("[!] dosya hatası.\n"));
-	printf("fd: %d\n", fd);
 	dup2(fd, 1);
 	close(fd);
 }
 
-void ms_run_infile(char **pt, int mod) {}
+void ms_run_infile(char **pt)
+{
+	int	fd;
+	fd = open(pt[2], O_RDONLY, 0777);
+	if (fd == -1)
+		return ((void)printf("[!] dosya hatası.\n"));
+	dup2(fd, 0);
+	close(fd);
+}
